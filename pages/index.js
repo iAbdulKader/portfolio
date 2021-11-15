c4829a6bd32e98f0d85cefdc3c5f3d016c20f13c
iAbdulKader <abdulkadernow@gmail.com>
2021-11-15 21:28:45 +0600

    Typo

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head';
 import DashBoard from '../components/Home';
 import About from '../components/About';
 import Technologies from '../components/Technologies';
+import Projects from '../components/Projects';
+import Blog from '../components/Blog';
+import Contact from '../components/Contact';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,9 +15,13 @@ export default function Home() {
         <meta name="description" content="Portfoilo of iAbdulKader" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <DashBoard />
       <About />
       <Technologies />
+      <Projects />
+      <Blog />
+      <Contact />
     </>
   )
 }
