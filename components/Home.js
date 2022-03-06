746ee36063c01635829b64bb57d2fc6199a6f9f3
iAbdulKader <abdulkadernow@gmail.com>
2022-03-06 14:32:37 +0600

    Animation Change
    Signed-off-by: iAbdulKader <abdulkadernow@gmail.com>

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,30 @@
 import styles from '../styles/Home.module.css';
-import Typewriter from 'typewriter-effect';
+import {useState, useEffect} from "react";
+import TextTransition, { presets } from "react-text-transition";
 
 export default function DashBoard() {
+  let strings = [
+    'ReactJS',
+    'NextJS',
+    'Redux',
+    'ExpressJS',
+    'MongoDB',
+    'Socet.io',
+    'Google API',
+    'AWS',
+    'GCP'
+  ]
+      
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() =>
+      setIndex(index => index + 1),
+      1500 // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
+  
   return (
     <>
       <div className={styles.container}>
@@ -10,25 +33,13 @@ export default function DashBoard() {
              <h3>I&apos;m Abdul Kader<span className={styles.redDot}>.</span></h3>
              <h3>A Passionate Full Stack Developer<span className={styles.redDot}>.</span></h3>
              <h4>Experienced In <span className={styles.typeText}>
-               <Typewriter
+               <TextTransition
+                  text={ strings[index % strings.length] }
+                  springConfig={ presets.wobbly }
+                  inline
                   className={styles.typeText}
-                  options={{
-                  strings: [
-                  'ReactJS',
-                  'NextJS',
-                  'Redux',
-                  'ExpressJS',
-                  'MongoDB',
-                  'Socet.io',
-                  'Google API',
-                  'AWS',
-                  'GCP'
-                  ],
-                  autoStart: true,
-                  loop: true,
-                  cursorClassName: "cursor"
-                }}
-               />
+              />
+              
              </span></h4>
         </div>
       </div>
